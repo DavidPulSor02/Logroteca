804bcbfba90d01a2ad5bb8d22971c75561204845
MarcoDP <delgadomarco2007@gmail.com>
2025-02-04 09:56:33 -0600

    cards-game

---
+++ src/app/components/cards-game/cards-game.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-game',
+  templateUrl: './cards-game.component.html',
+  styleUrls: ['./cards-game.component.scss'],
+})
+export class CardsGameComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
