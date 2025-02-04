3ae72fc04d1d0a6948ae1a187319d3a42ce9004d
MarcoDP <delgadomarco2007@gmail.com>
2025-02-04 09:40:09 -0600

    modified de components.module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent
+  ],
   imports: [
+    IonicModule,
     CommonModule
+  ],
+  exports: [
+    LayoutComponent
   ]
 })
 export class ComponentsModule { }
