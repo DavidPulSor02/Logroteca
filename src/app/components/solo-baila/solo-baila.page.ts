7329cee8fc52f1b6f0d551f71583a4d692978db2
MarcoDP <delgadomarco2007@gmail.com>
2025-02-10 07:54:44 -0600

    ola

--- src/app/components/solo-baila/solo-baila.page.ts
+++ src/app/components/solo-baila/solo-baila.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-solo-baila',
   templateUrl: './solo-baila.page.html',
   styleUrls: ['./solo-baila.page.scss'],
+  standalone: false
 })
 export class SoloBailaPage implements OnInit {
 
